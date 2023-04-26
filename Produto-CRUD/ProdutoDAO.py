f46a8fce6638eaf5662573ca69aa4bd7a41a0612
Lucasmtsoares <lmsl3@aluno.ifal.edu.br>
2023-04-26 00:09:36 -0300

    Update ProdutoDAO.py

--- Produto-CRUD/ProdutoDAO.py
+++ Produto-CRUD/ProdutoDAO.py
@@ -5,13 +5,14 @@ class ProdutoDAO:
 
     conexao = mongoDBConnection()
 
-    def salvar_produto(self, nome, preco,descricao, comentario):
-        produto = {'nome': nome,'preco': preco,'descricao':descricao, 'comentario': comentario}
+    def salvar_produto(self, nome, preco,descricao, avaliacao):
+        produto = {'nome': nome,'preco': preco,'descricao':descricao, 'comentario': avaliacao}
         result = self.conexao.produto.insert_one(produto)
         return result.inserted_id
 
-    def consultar_produto(id):
-        produto = conexao.produto.find_one({"_id": ObjectId(id)})
+    def consultar_produto(self,id):
+        produto = self.conexao.produto.find_one({"_id": ObjectId(id)})
         return produto
 
-    def atualizar_produto(self,)
+    def atualizar_produto(self,):
+
