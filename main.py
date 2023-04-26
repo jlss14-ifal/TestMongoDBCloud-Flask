a1655389e897143207f00c58b62858ba8184f0fe
Lucasmtsoares <88630261+Lucasmtsoares@users.noreply.github.com>
2023-04-26 00:31:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
+cont =1
 
-app = Flask(__name__)
+while(cont!=0):
+    print("[1] - Clientes")
+    print("[2] - Catálogos")
+    print("[3] - Produtos")
+    print("[4] - Comentários")
+    print("[0] - Sair")
+    opcao = input("Digite a sua opção: ")
 
-@app.route("/home")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    
+    if(opcao==3):
+        print("[1] - Cadastrar produto")
+        print("[2] - Consultar produto")
+        print("[3] - Atualizar produto")
+        print("[4] - Deletaar produtos")
 
-app.run()
+        opcaoP = input("Digite a sua opção: ")
+
+
+        #ogfoink
+    
